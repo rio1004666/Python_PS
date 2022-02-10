64c2c7f2e8e7a0d84c33a6be6d74fdee18f5a3bf
rio1004666 <rkdqudghk12@naver.com>
2022-02-10 21:24:47 +0900

    내적 - 문법 풀이 단순화

---
+++ 프로그래머스_내적.py
@@ -0,0 +1,15 @@
+# 내 풀이
+# def solution(a,b):
+#     answer = 0
+#     n = len(a)
+#     for i in range(n):
+#         answer += a[i]*b[i]
+
+
+# 다른 사람 풀이 => 파이썬은  list comprehension으로 바로 sum함수를 사용할 수 있다
+# zip 은 두개의 배열 각 원소를 튜플형태로 받을 수 있다
+
+def solution(a,b):
+    return sum([x*y for x,y in zip(a,b)])
+
+print(solution([1,2,3,4],[-3,-1,0,2]))
